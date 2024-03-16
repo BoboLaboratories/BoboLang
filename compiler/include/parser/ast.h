bdf6fdc12e6739096eeba12dd011f6103a848c65
Fabio Nebbia <fabionebbia97@gmail.com>
2024-03-16 17:15:12 +0100

    add license headers in source files
    Co-authored-by: Mattia Mignogna <68876886+Mattia249@users.noreply.github.com>

--- compiler/include/parser/ast.h
+++ compiler/include/parser/ast.h
@@ -1,3 +1,30 @@
+/*
+ * MIT License
+ *
+ * Copyright (C) 2024 BoboLabs.net
+ * Copyright (C) 2024 Mattia Mignogna (https://stami.bobolabs.net)
+ * Copyright (C) 2024 Fabio Nebbia (https://glowy.bobolabs.net)
+ * Copyright (C) 2024 Third party contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #ifndef BOBO_LANG_COMPILER_AST_H
 #define BOBO_LANG_COMPILER_AST_H
 
@@ -6,6 +33,22 @@
 
 #include "lib/data/arraylist/arraylist.h"
 
+/* Helper data structures */
+typedef struct {
+    bool is_private;
+    bool is_const;
+    char *name;
+} VariableSignature;
+
+typedef struct {
+    bool is_private;
+    bool is_native;
+    char *name;
+    ArrayList *args;
+} FunctionSignature;
+
+
+/* AST nodes */
 typedef struct {
     char *qid;
     ArrayList *args;
@@ -24,12 +67,6 @@ typedef struct {
     char *qid;
     ArrayList *args;
 } AST_StatInvoke;
-
-typedef struct {
-    bool is_private;
-    bool is_const;
-    char *name;
-} VariableSignature;
 
 typedef struct {
     VariableSignature *sig;
@@ -55,13 +92,6 @@ typedef struct {
     char *name;
     AST_Expr *expr;
 } AST_FunArg;
-
-typedef struct {
-    bool is_private;
-    bool is_native;
-    char *name;
-    ArrayList *args;
-} FunctionSignature;
 
 typedef struct {
     FunctionSignature *sig;
